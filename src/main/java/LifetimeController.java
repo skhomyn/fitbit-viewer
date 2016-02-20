2dfa9911aaf8556c590099168670e1fb0b94e87d
Robin Wakeman <rwakeman@uwo.ca>
2016-02-20 16:35:57 -0500

    ignore this I don't even remember what I changed

--- src/main/java/LifetimeController.java
+++ src/main/java/LifetimeController.java
@@ -1,7 +1,9 @@
 public class LifetimeController {
 	
 	//something something model object
+	LifetimeRecord model = new LifetimeRecord(); 
 	
 	//something something view object
+	LifetimeView view = new LifetimeView();
 	
 }
--- src/main/java/LifetimeRecord.java
+++ src/main/java/LifetimeRecord.java
@@ -1,10 +1,16 @@
 public class LifetimeRecord extends RecordModel {
 	
-	//date field?
 	private float distance;
 	private float floors;
 	private float steps;
 	
 	//constructor which populates fields with data from API call
+	//currently we are using fake sample data instead of an API call
+	LifetimeRecord( ) {
+		
+		
+	}
+	
+	//accessor methods
 	
 }
