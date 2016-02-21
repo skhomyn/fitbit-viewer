e5388c7d67eabc0e715c62f5ecc9afd2a739e380
Robin Wakeman <rwakeman@uwo.ca>
2016-02-21 03:00:54 -0500

    implemented lifetime floors feature with fake API and run with test class

--- src/main/java/FakeAPI.java
+++ src/main/java/FakeAPI.java
@@ -3,9 +3,14 @@ package sharkzilla;
 class FakeAPI {
 	
 	//for Lifetime Totals
-	private static float ltDistance = 14;
+	private static float ltDistance = 143;
+	private static int ltFloors = 54;
 	
 	public static float getLtDistance() {
 		return ltDistance;
 	}
+	public static int getLtFloors() {
+		return ltFloors;
+	}
+	
 }
--- src/main/java/LifetimeRecord.java
+++ src/main/java/LifetimeRecord.java
@@ -3,15 +3,20 @@ package sharkzilla;
 public class LifetimeRecord extends RecordModel {
 	
 	private float distance;
+	private int floors;
 	
 	//constructor which populates fields with data from API call
 	//currently we are using fake sample data instead of an API call
 	LifetimeRecord() {
 		distance = FakeAPI.getLtDistance();
+		floors = FakeAPI.getLtFloors();
 	}
 	
 	//access methods
 	public float getDistance() {
 		return distance;
+	}
+	public int getFloors() {
+		return floors;
 	}
 }
