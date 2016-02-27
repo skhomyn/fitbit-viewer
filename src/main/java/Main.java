52ef8e6e00a2464ebd6bfd8bc4320a5b3fc9d765
Andy Yu <ayu82@uwo.ca>
2016-02-27 00:23:15 -0500

    Fixed loading so it runs :D

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,25 +7,23 @@ import java.io.Reader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class App {
+public class Main {
 
-  public static void main(final String[] args) throws IOException {
+public void run() throws IOException {
+
     // Configure GSON
     final GsonBuilder gsonBuilder = new GsonBuilder();
     gsonBuilder.registerTypeAdapter(ActivitiesRecord.class, new ActivitiesRecordDeserializer());
     gsonBuilder.setPrettyPrinting();
     final Gson gson = gsonBuilder.create();
 
+
     // Read the JSON data
-    try (Reader data = new InputStreamReader(Main.class.getResourceAsStream("activities.json"), "UTF-8")) {
+ try (Reader data = new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("activities.json"), "UTF-8")) {
 
       // Parse JSON to Java
       final ActivitiesRecord actRecord = gson.fromJson(data, ActivitiesRecord.class);
       
-     // System.out.println(actRecord.getBest().getDis_date());
-     // System.out.println(actRecord.getLifetime());
-
-
 		LifetimeView ltView = new LifetimeView();
 
 		LifetimeRecord ltModel = actRecord.getLifetime();
@@ -34,5 +32,7 @@ public class App {
 		ltView.setVisible(true);
 
     }
+
+
   }
 }
--- src/main/java/ca/uwo/csd/cs2212/team13/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/App.java
@@ -1,46 +1,25 @@
-/*
-//package ca.uwo.csd.cs2212.team13;
- import org.apache.logging.log4j.LogManager;
- import org.apache.logging.log4j.Logger;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+package ca.uwo.csd.cs2212.team13;
+
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
   public class App {
   static Logger logger = LogManager.getLogger (App.class.getName());
-  public static void main ( String[] args ) throws IOException {
+
+  public static void main (String[] args ) throws IOException {
     logger.trace("Entering main");
     logger.warn("Hello Maven/log4j World"); 
     logger.info("My username is team13"); 
 
-    // Configure GSON
-    final GsonBuilder gsonBuilder = new GsonBuilder();
-    gsonBuilder.registerTypeAdapter(ActivitiesRecord.class, new ActivitiesRecordDeserializer());
-    gsonBuilder.setPrettyPrinting();
-    final Gson gson = gsonBuilder.create();
 
-    // Read the JSON data
-    try (Reader data = new InputStreamReader(Main.class.getResourceAsStream("activities.json"), "UTF-8")) {
-
-      // Parse JSON to Java
-      final ActivitiesRecord actRecord = gson.fromJson(data, ActivitiesRecord.class);
-
-		LifetimeView ltView = new LifetimeView();
-
-		LifetimeRecord ltModel = actRecord.getLifetime();
-		LifetimeController ltController = new LifetimeController(ltModel, ltView);
-		
-		ltView.setVisible(true);
-
-}
-
+	Main run = new Main();
+	run.run();
 
    logger.trace("Exiting main");  
 } 
 
 }
-*/
+
  
