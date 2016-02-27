9b1cc10fd71e55e454a92ea936d1c97d0c523135
Karthik Bhaskar <kbhaskar@uwo.ca>
2016-02-27 15:59:27 -0500

    Daily Record and Deserilizer first implementation

--- src/main/java/ca/uwo/csd/cs2212/team13/DailyRecordDeserializer.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/DailyRecordDeserializer.java
@@ -19,30 +19,18 @@ public class DailyRecordDeserializer implements JsonDeserializer<DailyRecord> {
 			throws JsonParseException {
 
 		final JsonObject jsonObject = json.getAsJsonObject();
-
-		//actually implement code
-/*	
-		final String best_dis_date = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("distance").get("date").getAsString();
-		final double best_dis_value = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("distance").get("value").getAsDouble();
-		final String best_floors_date = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("floors").get("date").getAsString();
-		final double best_floors_value = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("floors").get("value").getAsDouble();
-		final String best_steps_date = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("steps").get("date").getAsString();
-		final double best_steps_value = jsonObject.getAsJsonObject("best").getAsJsonObject("total").getAsJsonObject("steps").get("value").getAsDouble();
-
-		final int lt_aScore = jsonObject.getAsJsonObject("lifetime").getAsJsonObject("total").get("activeScore").getAsInt();
-		final int lt_cOut = jsonObject.getAsJsonObject("lifetime").getAsJsonObject("total").get("caloriesOut").getAsInt();
-		final double lt_distance = jsonObject.getAsJsonObject("lifetime").getAsJsonObject("total").get("distance").getAsDouble();
-		final int lt_floors = jsonObject.getAsJsonObject("lifetime").getAsJsonObject("total").get("floors").getAsInt();
-		final int lt_steps = jsonObject.getAsJsonObject("lifetime").getAsJsonObject("total").get("steps").getAsInt();
-
-		final LifetimeRecord lf = new LifetimeRecord(lt_aScore, lt_cOut, lt_distance, lt_floors, lt_steps);
-		final BestRecord br = new BestRecord(best_dis_date, best_dis_value, best_floors_date, best_floors_value, best_steps_date, best_steps_value);
-
-		final ActivitiesRecord actRecord = new ActivitiesRecord(br, lf);
-		System.out.println(actRecord.toString());
-		return actRecord;
-*/
+		final JsonObject jsonSummary = jsonObject.getAsJsonObject("summary");
 		
 		
+		
+		final DailyRecord dailyRecord = new DailyRecord();
+		DailyRecord.setFloors(jsonSummary.get("floors").getAsInt());
+		DailyRecord.setSteps(jsonSummary.get("steps").getAsInt());
+		DailyRecord.setLightlyActiveMinutes(jsonSummary.get("lightlyActiveMinutes").getAsInt());
+		DailyRecord.setFairlyActiveMinutes(jsonSummary.get("fairlyActiveMinutes").getAsInt());
+		DailyRecord.setSedentaryMinutes(jsonSummary.get("sedentaryActiveMinutes").getAsInt());
+		DailyRecord.setVeryActiveMinutes(jsonSummary.get("veryActiveMinutes").getAsInt());
+		
+		return dailyRecord;
 	}
 }
