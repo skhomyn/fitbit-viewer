06424984a04bdf72e4160452f31f88bff1059a6f
Ryan Dabir <rdabir@uwo.ca>
2016-02-27 18:21:25 -0500

    test commit

--- src/main/java/ca/uwo/csd/cs2212/team13/HRZController.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/HRZController.java
@@ -1,5 +1,5 @@
 package ca.uwo.csd.cs2212.team13;
 
 public class HRZController {
-	//make controller after an example is made (thx Johnny & Robin)
+	//will make controller after an example is made (thx Johnny & Robin)
 }
