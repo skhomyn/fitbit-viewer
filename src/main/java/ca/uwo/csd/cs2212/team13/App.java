bf64eb94e60b2005b672da3b3384ff9a4bed722e
Andy Yu <ayu82@uwo.ca>
2016-02-27 09:45:44 -0500

    Added test mode

--- src/main/java/ca/uwo/csd/cs2212/team13/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/App.java
@@ -1,11 +1,10 @@
-
 package ca.uwo.csd.cs2212.team13;
 
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-  public class App {
+public class App {
   static Logger logger = LogManager.getLogger (App.class.getName());
 
   public static void main (String[] args ) throws IOException {
@@ -13,13 +12,24 @@ import org.apache.logging.log4j.Logger;
     logger.warn("Hello Maven/log4j World"); 
     logger.info("My username is team13"); 
 
-
-	Main run = new Main();
-	run.run();
-
-   logger.trace("Exiting main");  
-} 
-
+    if (args.length == 0)
+    {
+      logger.trace("No arguments passed");
+      logger.warn("We need to build a program. And we have to do it quickly");
+      logger.warn("Run with argument test to test");
+    }
+    else if (args.length == 1)
+    {
+      logger.trace("Running test mode");
+      Main run = new Main();
+      run.run();
+    }
+    else
+    {
+      logger.trace("Unvalid arguments passed for now…");
+    }
+    logger.trace("Exiting main");  
+  }
 }
 
  
