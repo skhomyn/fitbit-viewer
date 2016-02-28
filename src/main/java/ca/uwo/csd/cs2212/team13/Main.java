c9d0310cf982bb58c0c0f8df9b5b2d2e97b1b815
Robin Wakeman <rwakeman@uwo.ca>
2016-02-28 14:15:28 -0500

    added controller layer to allow model info to be pushed to view -- builds and runs but view window doesn't open

--- src/main/java/ca/uwo/csd/cs2212/team13/Main.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/Main.java
@@ -14,36 +14,23 @@ public void run() throws IOException {
     // Configure GSON
     final GsonBuilder gsonBuilder = new GsonBuilder();
     gsonBuilder.registerTypeAdapter(ActivitiesRecord.class, new ActivitiesRecordDeserializer());
-    gsonBuilder.registerTypeAdapter(DailyRecord.class, new DailyRecordDeserializer());
-    gsonBuilder.registerTypeAdapter(Goals.class, new GoalsDeserializer());
     gsonBuilder.setPrettyPrinting();
     final Gson gson = gsonBuilder.create();
 
-    
-    try (Reader data = new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("date.json"), "UTF-8")) {
 
-        // Parse JSON to Java
-        final DailyRecord dRecord = gson.fromJson(data, DailyRecord.class);
-        
-        System.out.println(dRecord);
-
-      }
-    
     // Read the JSON data
  try (Reader data = new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("activities.json"), "UTF-8")) {
 
       // Parse JSON to Java
       final ActivitiesRecord actRecord = gson.fromJson(data, ActivitiesRecord.class);
       
-		LifetimeView ltView = new LifetimeView();
+		InterfaceView view = new InterfaceView();
 
 		LifetimeRecord ltModel = actRecord.getLifetime();
-		LifetimeController ltController = new LifetimeController(ltModel, ltView);
+		LifetimeController ltController = new LifetimeController(ltModel, view);
 		
-		ltView.setVisible(true);
 
     }
-
 
 
   }
