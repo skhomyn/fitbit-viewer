878c75185635f01b5889a5168e73a2c6ee630c45
Robin Wakeman <rwakeman@uwo.ca>
2016-03-02 15:22:54 -0500

    added comments to Lifetime and BestDays controllers

--- src/main/java/ca/uwo/csd/cs2212/team13/BestDaysController.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/BestDaysController.java
@@ -3,25 +3,46 @@ package ca.uwo.csd.cs2212.team13;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Java Object which passes information from the the <code>BestDaysRecord</code> object to the 
+ * <code>InterfaceView</code> object.
+ * <p>
+ * <code>BestDaysController</code> contains references to the model object {@link BestDaysRecord}, 
+ * which contains the data for the Best Days page, and the view object {@link InterfaceView}, 
+ * which implements the user interface for all pages.
+ * <p>
+ * The purpose of this class is to pass the data contained in the model <code>BestDaysRecord</code>
+ * to the view <code>InterfaceView</code>, so that it can be displayed to the user.
+ */
 public class BestDaysController {
 	
-	//properties
 	private BestDaysRecord model;
 	private InterfaceView view;
 
-	//constructor; the controller is initialized with a call from Main.java
 	public BestDaysController(BestDaysRecord model, InterfaceView view) {
 		this.model = model;
 		this.view = view;
 		
-		//call method in view to add an event listener to the lifetime totals button 
+		//call method in view to add an event listener to the best days button 
 		view.addClickListenerBestDays(new clickListener());
 	}
 	
-	//this is the class definition for the action listener object that we pass to the above method;
-	//it says that whenever an action is observed (click), set the view fields with the model info;
-	// this class is instantiated in the call above, when the controller is initialized
+	/**
+	 * Local class accessible only by the <code>BestDaysController</code> object.
+	 * It's purpose is to define which actions should be performed when the Best Days button
+	 * in <code>InterfaceView</code> is clicked by the user
+	 *
+	 * @see java.awt.event.ActionListener
+	 */
 	class clickListener implements ActionListener{
+		/** 
+		 * Sets the Lifetime Totals display fields in <code>InterfaceView</code> with values from the
+		 * appropriate model <code>BestDaysRecord</code> fields upon the click of a button.
+		 * 
+		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+		 * 
+		 * @param click action on button
+		 */
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			view.setBestDaysFields(model.getDis_value(), model.getFloors_value(), model.getSteps_value(), model.getDis_date(), model.getFloors_date(), model.getSteps_date());
@@ -29,6 +50,8 @@ public class BestDaysController {
 		}
 	}
 	
-	//error handling to be added
+	/**
+	 * TODO: error handling
+	 */
  }	
 	
