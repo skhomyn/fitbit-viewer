27cf6eddd330c89a5a5c0b4c240e86bbd613ea1d
Hannah Russwurm <hrusswur@uwo.ca>
2016-03-02 21:04:14 -0500

    Goals Comments

--- src/main/java/ca/uwo/csd/cs2212/team13/Goals.java
+++ src/main/java/ca/uwo/csd/cs2212/team13/Goals.java
@@ -1,105 +1,151 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2016, Team Sharkzilla (13)
  */
 package ca.uwo.csd.cs2212.team13;
 
 /**
- *
- * @author kbhaskar
+ * <code>Goals</code> is a simple Java object that represents the
+ * goals and the rules that govern access to and updates of this data.
  */
 public class Goals {
     
+	/**
+	 * The active minutes goal
+	 */
     private int activeMinutes;
+    /**
+	 * The calories goal
+	 */
     private double caloriesOut;
+    /**
+	 * The distance goal
+	 */
     private double distance;
+    /**
+   	 * The floors goal
+   	 */
     private int floors;
+    /**
+   	 * The steps goal
+   	 */
     private int steps;
 
-    /**
-     * @return the activeMinutes
-     */
+    
+    
+	/**
+	 * Gets the <code>active minutes goal</code> field.
+	 * <p>
+	 * This method returns the {@code int} value for the distance.
+	 * 
+	 * @return the active minutes goal
+	 */
     public int getActiveMinutes() {
         return activeMinutes;
     }
 
     /**
-     * @return the caloriesOut
-     */
+	 * Gets the <code>calories goal</code> field.
+	 * <p>
+	 * This method returns the {@code double} value for the distance.
+	 * 
+	 * @return the calories goal
+	 */
     public double getCaloriesOut() {
         return caloriesOut;
     }
 
     /**
-     * @return the distance
-     */
+   	 * Gets the <code>distance goal</code> field.
+   	 * <p>
+   	 * This method returns the {@code double} value for the distance.
+   	 * 
+   	 * @return the distance goal
+   	 */
     public double getDistance() {
         return distance;
     }
 
     /**
-     * @return the floors
-     */
+   	 * Gets the <code>floors goal</code> field.
+   	 * <p>
+   	 * This method returns the {@code int} value for the distance.
+   	 * 
+   	 * @return the floors goal
+   	 */
     public int getFloors() {
         return floors;
     }
 
     /**
-     * @return the steps
-     */
+   	 * Gets the <code>steps goal</code> field.
+   	 * <p>
+   	 * This method returns the {@code int} value for the distance.
+   	 * 
+   	 * @return the steps goal
+   	 */
     public int getSteps() {
         return steps;
     }
 
     /**
-     * @param activeMinutes the activeMinutes to set
-     */
+	 * Sets the {@code active minutes} field.
+	 * <p>
+	 * This method sets the {@code int} value for the field {@code distance}.
+	 * 
+	 * @param active minutes
+	 *            the new value of the field {@code active minutes} for the goal
+	 */
     public void setActiveMinutes(int activeMinutes) {
         this.activeMinutes = activeMinutes;
     }
 
     /**
-     * @param caloriesOut the caloriesOut to set
-     */
+   	 * Sets the {@code calories out} field.
+   	 * <p>
+   	 * This method sets the {@code double} value for the field {@code distance}.
+   	 * 
+   	 * @param calories out
+   	 *            the new value of the field {@code calories} for the goal
+   	 */
     public void setCaloriesOut(double caloriesOut) {
         this.caloriesOut = caloriesOut;
     }
 
     /**
-     * @param distance the distance to set
-     */
+   	 * Sets the {@code distance} field.
+   	 * <p>
+   	 * This method sets the {@code double} value for the field {@code distance}.
+   	 * 
+   	 * @param distance
+   	 *            the new value of the field {@code distance} for the goal
+   	 */
     public void setDistance(double distance) {
         this.distance = distance;
     }
 
     /**
-     * @param floors the floors to set
-     */
+   	 * Sets the {@code floors} field.
+   	 * <p>
+   	 * This method sets the {@code int} value for the field {@code distance}.
+   	 * 
+   	 * @param floors
+   	 *            the new value of the field {@code floors} for the goal
+   	 */
     public void setFloors(int floors) {
         this.floors = floors;
     }
 
     /**
-     * @param steps the steps to set
-     */
+   	 * Sets the {@code steps} field.
+   	 * <p>
+   	 * This method sets the {@code int} value for the field {@code distance}.
+   	 * 
+   	 * @param steps
+   	 *            the new value of the field {@code steps} for the goal
+   	 */
     public void setSteps(int steps) {
         this.steps = steps;
     }
     
-    @Override
-	  public String toString() {
-		  final StringBuilder formatted = new StringBuilder();
-		    formatted.append(steps);
-		    formatted.append("\n");
-		    formatted.append(floors);
-		    formatted.append("\n");
-		    formatted.append(caloriesOut);
-		    formatted.append("\n");
-		    formatted.append(activeMinutes);
-		    formatted.append("\n");
-		    formatted.append(distance);
-		    return formatted.toString();
-		  }
-    
+  
 }
