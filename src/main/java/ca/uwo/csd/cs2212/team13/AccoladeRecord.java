8e5a4a33ed939d376dffde22d52e6d0486ad7020
Jenni Street <jstreet2@uwo.ca>
2016-03-10 17:14:36 -0500

    Very first draft of AccoladeRecord

---
+++ src/main/java/ca/uwo/csd/cs2212/team13/AccoladeRecord.java
@@ -0,0 +1,17 @@
+/* Copyright (c) 2016, Team Sharkzilla (13) */
+package ca.uwo.csd.cs2212.team13;
+
+public class AccoladeRecord {
+	
+	//properties go here
+	//one for each accolade? check API
+	
+	/** 
+	 * Constructor
+	 */
+	public AccoladeRecord() {
+		//what even goes here
+	}
+	
+	//getters & setters
+}
