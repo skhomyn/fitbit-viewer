701f978d583785793c71394616aca40e05e389a4
Andy Yu <ayu82@uwo.ca>
2016-03-11 19:58:11 -0500

    added WriterReader.java

---
+++ src/main/java/ca/uwo/csd/cs2212/team13/WriterReader.java
@@ -0,0 +1,27 @@
+package ca.uwo.csd.cs2212.team13;
+
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class WriterReader{
+
+	  public void writeRecord(Object standard, String filename) throws Exception {
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src/main/resources/" + filename));
+		out.writeObject(standard);
+		out.close();
+	  }
+
+	 public Object loadRecord(String filename) throws Exception {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+			Object object = in.readObject();
+			in.close();	
+			return object;
+	 }
+}
